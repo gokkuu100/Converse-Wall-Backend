0a5adeb861283e3a5f106d0c70a97b90d5797da1
gokkuu100 <wkurts247@gmail.com>
2023-11-27 18:59:56 +0300

    implemented sococket.io config

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
+const socketIO = require('socket.io');
 const router = require('./app/routes/routes')
 const bodyParser = require('body-parser');
 require('dotenv').config();
@@ -10,15 +12,15 @@ const app = express()
 app.use(cors())
 
 const db = require('./app/models');
-const controller = require('./app/controllers/users.controller');
-
 db.sequelize.sync().then(() => {
     console.log("Database synced successfully");
 });
-
 db.sequelize.sync();
 
 app.use(bodyParser.json())
+
+const server = http.createServer(app) // creates http server instance
+const io = socketIO(server); // attaches socket.io to the server
 
 // parse request for content-type application/json
 app.use(express.json())
@@ -37,6 +39,22 @@ const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// Socket.IO logiv
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Handle chat messages
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); // Broadcast the message to all connected clients
+    });
+
+    // Handle disconnect
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
 
 
 
