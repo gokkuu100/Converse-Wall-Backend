2b1eb28b29c461dc9f8825f44d9840f542f53b17
gokkuu100 <wkurts247@gmail.com>
2023-12-07 13:09:08 +0300

    socket.io broadcasting

--- index.js
+++ index.js
@@ -1,60 +1,54 @@
 const express = require('express');
 const cors = require('cors');
-const http = require('http');
+const http = require('http');  // Import http module
 const socketIO = require('socket.io');
-const router = require('./app/routes/routes')
+const router = require('./app/routes/routes');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-
-
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
 const db = require('./app/models');
 db.sequelize.sync().then(() => {
     console.log("Database synced successfully");
 });
-db.sequelize.sync();
 
-app.use(bodyParser.json())
-
-const server = http.createServer(app) // creates http server instance
-const io = socketIO(server); // attaches socket.io to the server
-
-// parse request for content-type application/json
-app.use(express.json())
-
-// parse requests of content-type -application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', router.router);
 
-// simple route
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the API' });
 });
 
-const PORT = process.env.PORT || 8080
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+const server = http.createServer(app); 
+
+// Use server instance for Socket.IO
+const io = socketIO(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
 });
 
-// Socket.IO logiv
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    console.log(`User connected: ${socket.id}`);
 
-    // Handle chat messages
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg); // Broadcast the message to all connected clients
+    socket.on('send_message', (data) => {
+        socket.broadcast.emit('receive_message', data);
     });
 
-    // Handle disconnect
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
 });
 
+const PORT = process.env.PORT || 8080;
 
-
-
+// Use server.listen instead of app.listen
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
